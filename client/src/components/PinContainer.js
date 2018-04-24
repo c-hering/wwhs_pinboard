d636eb0d7302f63a80dad7d35570f95cd9dcfe48
An0rak <phos4es@gmail.com>
2018-04-24 18:29:45 -0400

    apparently react doesn't allow for the use of objects for storing data, which makes using practice data impossible without using sqlite, so therefore time to move to sqlite!

--- client/src/components/PinContainer.js
+++ client/src/components/PinContainer.js
@@ -3,6 +3,14 @@ import { Panel, Table, Well } from 'react-bootstrap';
 import MessageRow from './MessageRow';
 export default class PinContainer extends Component {
 
+  constructor(props) {
+      super(props);
+      this.state = {
+        practiceData: []
+      };
+  }
+
+
   rateUp = msgID => {
     console.log(msgID + " was pressed")
   }
@@ -13,7 +21,7 @@ export default class PinContainer extends Component {
 
   getRowData = () => {
     var messages = [];
-    for(var i = 0; i < 50; i++){
+    for(var i = 0; i < 5; i++){
       messages.push(<MessageRow key={i} onRateUp={() => this.rateUp()} onRateDown={() => this.rateDown(i)} id={i} rating={Math.floor(Math.random() * 400)} message="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />)
     }
     return <tbody>{messages}</tbody>;
