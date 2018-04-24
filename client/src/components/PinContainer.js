12d250ad2682c6e96c3050a9a7c0e23dd397e191
An0rak <phos4es@gmail.com>
2018-04-24 18:04:41 -0400

    working on making practice data

--- client/src/components/PinContainer.js
+++ client/src/components/PinContainer.js
@@ -14,7 +14,7 @@ export default class PinContainer extends Component {
   getRowData = () => {
     var messages = [];
     for(var i = 0; i < 50; i++){
-      messages.push(<MessageRow key={i} onRateUp={() => this.rateUp(i)} onRateDown={() => this.rateDown(i)} id={i} rating="142" message="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />)
+      messages.push(<MessageRow key={i} onRateUp={() => this.rateUp()} onRateDown={() => this.rateDown(i)} id={i} rating={Math.floor(Math.random() * 400)} message="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />)
     }
     return <tbody>{messages}</tbody>;
   }
