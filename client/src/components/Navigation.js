2694e71cc3f05e51852991577e065d73aa793438
c-hering <phos4es@gmail.com>
2018-04-25 18:01:18 +0000

    changed nav

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -12,8 +12,11 @@ export default class Navigation extends Component {
           </Navbar.Brand>
         </Navbar.Header>
         <Nav>
-          <NavItem eventKey={1} href="/hottest">
-            Hottest
+          <NavItem eventKey={1} href="/">
+	   By Newest
+	  </NavItem>
+	  <NavItem eventKey={1} href="/hottest">
+            By Popular
           </NavItem>
         </Nav>
       </Navbar>
