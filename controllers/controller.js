80be429cce7c989900ece53650f15c66a4518ae0
An0rak <phos4es@gmail.com>
2018-04-24 21:48:25 -0400

    added POST response to /messages, TODO: error handling for POST to /messages, error handling for GET to /messages

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,3 +1,6 @@
+var sqlite3 = require('sqlite3').verbose()
+var db = new sqlite3.Database('./serverdb.db')
+
 exports.home = (req,res) => {
 	console.log("There must be some mistake!");
 	res.send("WWHS Pinboard!")
@@ -5,10 +8,37 @@ exports.home = (req,res) => {
 
 exports.newMessage = (req,res) => {
 	console.log(req.body);
-	res.send("<Response><Message>Thanks for contributing to the pinboard!</Message></Response>");
+
+	if(req.body !== {}){
+		var id = req.body.id;
+		var rating = req.body.rating;
+		var message = req.body.msg;
+
+		console.log(id)
+
+		db.serialize(() => {
+			db.run("INSERT INTO messages (ID,RATING,MSG) \ VALUES (" + id + "," + rating + "," + message + ")")
+		})
+		res.send("Message Added!")
+	}else{
+		console.log("ERR: empty body in post req")
+		res.send("Error adding message, seems you didn't send anything!")
+	}
+
+
+	// for twilio only
+	// res.send("<Response><Message>Thanks for contributing to the pinboard!</Message></Response>");
 };
 
 exports.getMessages = (req,res) => {
 	console.log("connection made for tester");
+
+	db.serialize(() => {
+		db.run('CREATE TABLE IF NOT EXISTS messages (ID INT PRIMARY KEY NOT NULL,RATING INT NOT NULL,MSG TEST NOT NULL);')
+		db.each('SELECT id, rating, msg FROM messages', (err,row) => {
+			console.log(row)
+		})
+	})
+
 	res.send("Test Successful!");
 };
--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var express = require('express'),
 	app = express(),
 	port = process.env.PORT || 3001;
 var routes = require("./routes/routehandler");
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 routes(app)
 app.listen(port);
