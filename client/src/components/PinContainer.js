b3888f3925df96f8cd69b0cfca97d65bd025bbfb
An0rak <phos4es@gmail.com>
2018-04-25 18:10:51 -0400

    got rid of deprecated function

--- client/src/components/PinContainer.js
+++ client/src/components/PinContainer.js
@@ -17,10 +17,6 @@ export default class PinContainer extends Component {
     console.log(msgID + " was pressed")
   }
 
-  getRowData = () => {
-
-  }
-
   renderSortBy = sortby => {
     if(sortby == 'newest'){
       console.log("Sorting messages by newest")
