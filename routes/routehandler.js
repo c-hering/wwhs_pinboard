32af9decf5f3d49782bdd4f195cbd00fc46f6530
An0rak <phos4es@gmail.com>
2018-05-05 11:17:56 -0400

    cors?

--- routes/routehandler.js
+++ routes/routehandler.js
@@ -4,7 +4,7 @@ module.exports = app => {
 	app.route('/').get(controller.home);
 	app.route('/messages/:order/:page').get(controller.getMessages);
 	// app.route('/rating').get(controller.getRating);
-	app.route('/deleteMessages').get(controller.deleteMessages);
+	//app.route('/deleteMessages').get(controller.deleteMessages);
 
 	app.route('/messages').post(controller.newMessage);
 	app.route('/rating').post(controller.handleRating);
