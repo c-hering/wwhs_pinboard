78d2b5575792a8f5e58279516e1f1c47caac7147
An0rak <phos4es@gmail.com>
2018-05-09 17:00:35 -0400

    added page count

--- client/src/components/PageNav.js
+++ client/src/components/PageNav.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 
 export default class PageNav extends Component {
 
   render(){
     return(
-      
+      <Button onClick={this.props.onPress} bsStyle="link">Load more...</Button>
     );
   }
 };
